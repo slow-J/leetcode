0d2fe81ab02fecfa741725588f55d17c73d6b206
slow-J <32519034+slow-J@users.noreply.github.com>
2021-02-08 20:14:32 +0000

    Add files via upload

--- easy/509.fibonacci_number.py
+++ easy/509.fibonacci_number.py
@@ -1,6 +1,30 @@
 class Solution:
-    # def fib(self, N: int) -> int:
+    # naive way, O(2^N) time, O(N) space
     def recursive_fib(self, N):
         if N < 2:
             return N
         return self.recursive_fib(N - 1) + self.recursive_fib(N - 2)
+
+    # memoization recursive, O(N) time, O(N) space
+    def fib_memo_rec(self, n: int, memo: dict) -> int:
+        if n == 0:
+            return 0
+        elif n <= 2:
+            return 1
+        elif n in memo:
+            return memo[n]
+        m = self.fib_memo_rec(n - 1, memo) + self.fib_memo_rec(n - 2, memo)
+        memo[n] = m
+        return m
+
+    # top-down, O(N) time, O(1) space
+    def fib_memo(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n <= 2:
+            return 1
+        minus_1 = 1
+        minus_2 = 1
+        for x in range(n - 1):
+            minus_2, minus_1 = minus_1, minus_1 + minus_2
+        return minus_2
