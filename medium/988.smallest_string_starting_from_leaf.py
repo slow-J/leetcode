9519b4c5fd0f3a4b8434c3d4c1d8130db42b7f8e
Jakub Slowinski <32519034+slow-J@users.noreply.github.com>
2021-03-03 12:15:25 +0000

    Add files via upload

--- medium/988.smallest_string_starting_from_leaf.py
+++ medium/988.smallest_string_starting_from_leaf.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
+class Solution1:
     """Iterative BFS with 1 queue of tuples.
     Runtime: 32 ms, faster than 99.79%
     Memory Usage: 15.8 MB, less than 13.25%
@@ -24,6 +24,36 @@ class Solution:
                 stack.append((cur_node.right, new_path))
             if not cur_node.left and not cur_node.right:
                 found_paths.append(new_path)
+
+        smallest_path = None
+        for path in found_paths:
+            if not smallest_path:
+                smallest_path = path
+            else:
+                smallest_path = min(path, smallest_path)
+        return smallest_path
+
+
+class Solution2:
+    """Iterative DFS with 1 queue of tuples. Better memory usage than BFS.
+    Runtime: 40 ms, faster than 94.41%
+    Memory Usage: 15.6 MB, less than 78.26%
+    """
+
+    def smallestFromLeaf(self, root: TreeNode) -> List[str]:
+        if not root:
+            return root
+        stack = deque([(root, "")])
+        found_paths = deque()
+        while stack:
+            cur_node, paths = stack.popleft()
+            new_string = chr((cur_node.val) + 97) + paths
+            if cur_node.right:
+                stack.appendleft((cur_node.right, new_string))
+            if cur_node.left:
+                stack.appendleft((cur_node.left, new_string))
+            if not cur_node.left and not cur_node.right:
+                found_paths.append(new_string)
 
         smallest_path = None
         for path in found_paths:
